0d80f95e51904904d234f7e2084b192e64b06312
Venkatesh Mishra <101781981+Soullinux@users.noreply.github.com>
2022-03-17 09:08:52 +0530

    Create welcome.py

---
+++ pkg/welcome.py
@@ -0,0 +1,17 @@
+# (C) Copyright 2022-2027 Venkatesh Mishra
+# Soul Linux welcome app
+
+
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Soul Linux greeter")
+    root.geometry("412x226")
+    root.mainloop()
+
+if __name__ == "__main__":
+    for i in range(1):
+        main()
+else:
+    pass
